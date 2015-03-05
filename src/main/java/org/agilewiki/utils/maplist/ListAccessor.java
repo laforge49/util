6e49393dc9f28d7659f3b64448abdf9916f22f6b
Bill La Forge <laforge49@gmail.com>
2015-03-05 16:18:04 -0500

    #6 started ListAccessor

---
+++ src/main/java/org/agilewiki/utils/maplist/ListAccessor.java
@@ -0,0 +1,46 @@
+package org.agilewiki.utils.maplist;
+
+import java.util.List;
+
+/**
+ * Accesses a versioned list for a given time.
+ */
+public interface ListAccessor {
+
+    /**
+     * Returns the time being accessed.
+     *
+     * @return The time being accessed.
+     */
+    long time();
+
+    /**
+     * Returns a value if it is in range and the value exists for the given time.
+     *
+     * @param ndx  The index of the selected value.
+     * @return A value, or null.
+     */
+    Object get(int ndx);
+
+    /**
+     * Returns the index of an existing value larger than the given index.
+     *
+     * @param ndx    A given index.
+     * @return An index of an existing value that is higher, or -1.
+     */
+    int higher(int ndx);
+
+    /**
+     * Returns true if there are no values present for the given time.
+     *
+     * @return Returns true if the list is empty for the given time.
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns a list of all the values that are present for a given time.
+     *
+     * @return A list of all values present for the given time.
+     */
+    List flat();
+}
--- src/main/java/org/agilewiki/utils/maplist/ListNode.java
+++ src/main/java/org/agilewiki/utils/maplist/ListNode.java
@@ -1,9 +1,20 @@
 package org.agilewiki.utils.maplist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A node in an AA Tree representing a versioned list.
  */
 public class ListNode {
+
+    private final static long MAX_TIME = Integer.MAX_VALUE - 1;
+
+    /**
+     * The root node of an empty tree.
+     */
+    public final static ListNode LIST_NIL = new ListNode();
+
     private int level;
     private ListNode leftNode;
     private ListNode rightNode;
@@ -11,8 +22,6 @@ public class ListNode {
     private final long created;
     private long deleted;
     private int size;
-
-    public final static ListNode LIST_NIL = new ListNode();
 
     private ListNode() {
         leftNode = this;
@@ -46,6 +55,14 @@ public class ListNode {
         return size;
     }
 
+    private boolean exists(long time) {
+        return time >= created && time < deleted;
+    }
+
+    private boolean isNil() {
+        return this == LIST_NIL;
+    }
+
     /**
      * Returns a value if it is in range and the value exists for the given time.
      *
@@ -61,8 +78,100 @@ public class ListNode {
             return leftNode.get(ndx, time);
         if (ndx > leftSize)
             return rightNode.get(ndx - leftSize - 1, time);
-        if (time < created || time >= deleted)
-            return null;
-        return value;
+        return exists(time) ? value : null;
+    }
+
+    public int higher(int ndx, long time) {
+        if (ndx < -1 || ndx >= size)
+            return -1; //out of range
+        int leftSize = leftNode.size;
+        if (ndx < leftSize + 1) {
+            int h = leftNode.higher(ndx, time);
+            if (h > -1)
+                return h;
+        }
+        if (ndx < leftSize + 2) {
+            if (exists(time))
+                return leftSize + 1;
+        }
+        return rightNode.higher(ndx - leftSize - 1, time) + leftSize + 1;
+    }
+
+    /**
+     * Returns true if there are no values present for the given time.
+     *
+     * @param time The time of the query.
+     * @return Returns true if the list is empty for the given time.
+     */
+    public boolean isEmpty(long time) {
+        if (isNil())
+            return true;
+        return !(exists(time) || leftNode.isEmpty(time) || rightNode.isEmpty(time));
+    }
+
+    private void flat(List list, long time) {
+        if (isNil())
+            return;
+        leftNode.flat(list, time);
+        if (exists(time))
+            list.add(value);
+        rightNode.flat(list, time);
+    }
+
+    /**
+     * Returns a list of all the values that are present for a given time.
+     *
+     * @param time The time of the query.
+     * @return A list of all values present for the given time.
+     */
+    public List flat(long time) {
+        List list = new ArrayList<>();
+        flat(list, time);
+        return list;
+    }
+
+    /**
+     * Returns a list accessor for the latest time.
+     *
+     * @return A list accessor for the latest time.
+     */
+    public ListAccessor accessor() {
+        return accessor(MAX_TIME);
+    }
+
+    /**
+     * Returns a list accessor for the given time.
+     *
+     * @param time    The time of the query.
+     * @return A list accessor for the given time.
+     */
+    public ListAccessor accessor(long time) {
+        return new ListAccessor() {
+
+            @Override
+            public long time() {
+                return time;
+            }
+
+            @Override
+            public Object get(int ndx) {
+                return ListNode.this.get(ndx, time);
+            }
+
+            @Override
+            public int higher(int ndx) {
+                return ListNode.this.higher(ndx, time);
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return ListNode.this.isEmpty(time);
+            }
+
+            @Override
+            public List flat() {
+                return ListNode.this.flat(time);
+            }
+        };
     }
 }
