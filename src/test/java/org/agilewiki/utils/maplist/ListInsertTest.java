ec4e0e78fdab9a737028526f08a342b43abac590
Bill La Forge <laforge49@gmail.com>
2015-03-05 18:51:40 -0500

    #6 progress on ListInsertTest

--- src/test/java/org/agilewiki/utils/maplist/ListInsertTest.java
+++ src/test/java/org/agilewiki/utils/maplist/ListInsertTest.java
@@ -4,13 +4,38 @@ import junit.framework.TestCase;
 
 public class ListInsertTest extends TestCase {
     public void test() throws Exception {
-        ListNode ln1 = ListNode.LIST_NIL.add("a", 2);
-        ln1 = ln1.add("b", 3);
-        ln1 = ln1.add("c", 4);
-        ln1 = ln1.add("d", 5);
-        ln1 = ln1.add("e", 6);
-        ln1 = ln1.add("f", 7);
-        ln1 = ln1.add("g", 8);
-        assertEquals(7,ln1.maxSize());
+        ListNode l1 = ListNode.LIST_NIL.add("a", 2);
+        l1 = l1.add("b", 3);
+        l1 = l1.add("c", 4);
+        l1 = l1.add("d", 5);
+        l1 = l1.add("e", 6);
+        l1 = l1.add("f", 7);
+        l1 = l1.add("g", 8);
+        assertEquals(7,l1.maxSize());
+        ListAccessor a1 = l1.accessor();
+        assertEquals("a", a1.get(0));
+        assertEquals("b", a1.get(1));
+        assertEquals("c", a1.get(2));
+        assertEquals("d", a1.get(3));
+        assertEquals("e", a1.get(4));
+        assertEquals("f", a1.get(5));
+        assertEquals("g", a1.get(6));
+
+        ListNode l2 = ListNode.LIST_NIL.add(0, "G", 2);
+        l2 = l2.add(0, "F", 3);
+        l2 = l2.add(0, "E", 4);
+        l2 = l2.add(0, "D", 5);
+        l2 = l2.add(0, "C", 6);
+        l2 = l2.add(0, "B", 7);
+        l2 = l2.add(0, "A", 8);
+        assertEquals(7,l2.maxSize());
+        ListAccessor a2 = l2.accessor();
+        assertEquals("A", a2.get(0));
+        assertEquals("B", a2.get(1));
+        assertEquals("C", a2.get(2));
+        assertEquals("D", a2.get(3));
+        assertEquals("E", a2.get(4));
+        assertEquals("F", a2.get(5));
+        assertEquals("G", a2.get(6));
     }
 }
