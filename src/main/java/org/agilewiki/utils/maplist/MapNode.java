1d5a93f54149809b90a1b7f0d2162f3b6999377e
Bill La Forge <laforge49@gmail.com>
2015-03-07 16:00:58 -0500

    #6 started MapNode add

--- src/main/java/org/agilewiki/utils/maplist/MapNode.java
+++ src/main/java/org/agilewiki/utils/maplist/MapNode.java
@@ -66,4 +66,40 @@ public class MapNode {
         }
         return this;
     }
+
+    /**
+     * Add a non-null value to the list.
+     * After calling add, a previously created accessor becomes invalid.
+     *
+     * @param key      The key of the list.
+     * @param ndx   Where to add the value.
+     * @param value The value to be added.
+     * @param time  The time the value is added.
+     * @return The revised root node.
+     */
+    public MapNode add(Comparable key, int ndx, Object value, long time) {
+        return add(key, ndx, value, time, Integer.MAX_VALUE);
+    }
+
+    protected MapNode add(Comparable key, int ndx, Object value, long created, long deleted) {
+        if (key == null)
+            throw new IllegalArgumentException("key may not be null");
+        if (value == null)
+            throw new IllegalArgumentException("value may not be null");
+        if (isNil()) {
+            if (ndx != 0)
+                throw new IllegalArgumentException("index out of range");
+            ListNode listNode = ListNode.LIST_NIL.add(0, value, created, deleted);
+            return new MapNode(1, MAP_NIL, MAP_NIL, listNode, key);
+        }
+        int c = key.compareTo(this.key);
+        if (c < 0)
+            leftNode = leftNode.add(key, ndx, value, created, deleted);
+        else if (c == 0) {
+            this.value = this.value.add(ndx, value, created, deleted);
+            return this;
+        } else
+            rightNode = rightNode.add(key, ndx, value, created, deleted);
+        return skew().split();
+    }
 }
