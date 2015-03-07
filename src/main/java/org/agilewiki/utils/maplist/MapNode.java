200002e515239197f85157e2b33ba42b5218e365
Bill La Forge <laforge49@gmail.com>
2015-03-07 14:14:30 -0500

    #6 started MapNode

--- src/main/java/org/agilewiki/utils/maplist/MapNode.java
+++ src/main/java/org/agilewiki/utils/maplist/MapNode.java
@@ -6,4 +6,35 @@ package org.agilewiki.utils.maplist;
  * representing a map of versioned lists.
  */
 public class MapNode {
+    /**
+     * The root node of an empty tree.
+     */
+    public final static MapNode MAP_NIL = new MapNode();
+
+    protected int level;
+    protected MapNode leftNode;
+    protected MapNode rightNode;
+    protected ListNode value;
+    protected Comparable key;
+
+    protected MapNode() {
+        leftNode = this;
+        rightNode = this;
+    }
+
+    protected MapNode(int level,
+                      MapNode leftNode,
+                      MapNode rightNode,
+                       ListNode value,
+                       Comparable key) {
+        this.level = level;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+        this.value = value;
+        this.key = key;
+    }
+
+    protected boolean isNil() {
+        return this == MAP_NIL;
+    }
 }
