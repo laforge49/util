d26f7dac01ab8249151fb062ed4a6851f333f7f0
Bill La Forge <laforge49@gmail.com>
2015-03-08 11:51:17 -0400

    #6 start MapRemoveTest

--- src/test/java/org/agilewiki/utils/maplist/MapRemoveTest.java
+++ src/test/java/org/agilewiki/utils/maplist/MapRemoveTest.java
@@ -2,12 +2,13 @@ package org.agilewiki.utils.maplist;
 
 import junit.framework.TestCase;
 
-public class MapInsertTest extends TestCase {
+public class MapRemoveTest extends TestCase {
     public void test() throws Exception {
+        assertNull(MapNode.MAP_NIL.remove("", 0, 1));
 
         MapNode m1 = MapNode.MAP_NIL.add("1", "a", 2);
+        assertEquals("a", m1.remove("1", 0, 3));
 
         assertEquals(1, m1.maxSize("1"));
-
     }
 }
