bfb92c36029128fa6515ac2e3d257c1178296f23
Bill La Forge <laforge49@gmail.com>
2015-03-15 06:29:40 -0400

    #14 added javadocs to durable list node data

--- src/main/java/org/agilewiki/utils/lazydurable/DurableListNodeData.java
+++ src/main/java/org/agilewiki/utils/lazydurable/DurableListNodeData.java
@@ -6,16 +6,51 @@ import java.nio.ByteBuffer;
  * The durable data elements of a list node.
  */
 public class DurableListNodeData {
+    /**
+     * Composite node depth--see AA Tree algorithm.
+     */
     public final int level;
+
+    /**
+     * Number of nodes in this subtree.
+     */
     public final int totalSize;
+
+    /**
+     * Creation time of this node.
+     */
     public final long created;
+
+    /**
+     * Deletion time of this node.
+     */
     public final long deleted;
+
+    /**
+     * Left subtree node.
+     */
     public final LazyDurableListNode leftNode;
+
+    /**
+     * The value of the node.
+     */
     public final Object value;
+
+    /**
+     * Right subtree node.
+     */
     public final LazyDurableListNode rightNode;
 
+    /**
+     * The factory for the value.
+     */
     protected final LazyDurableFactory valueFactory;
 
+    /**
+     * Create the nil node data.
+     *
+     * @param thisNode    The nil node.
+     */
     public DurableListNodeData(LazyDurableListNode thisNode) {
         this.level = 0;
         totalSize = 0;
@@ -28,6 +63,17 @@ public class DurableListNodeData {
         valueFactory = null;
     }
 
+    /**
+     * Create non-nill node data.
+     *
+     * @param level        Composite node depth--see AA Tree algorithm.
+     * @param totalSize    Number of nodes in this subtree.
+     * @param created      Creation time of this node.
+     * @param deleted      Deletion time of this node.
+     * @param leftNode     Left subtree node.
+     * @param value        The value of the node.
+     * @param rightNode    Right subtree node.
+     */
     public DurableListNodeData(int level,
                                int totalSize,
                                long created,
@@ -45,6 +91,11 @@ public class DurableListNodeData {
         this.valueFactory = FactoryRegistry.getDurableFactory(value);
     }
 
+    /**
+     * Create non-nill node data.
+     *
+     * @param byteBuffer    Holds the serialized data.
+     */
     public DurableListNodeData(ByteBuffer byteBuffer) {
         level = byteBuffer.getInt();
         totalSize = byteBuffer.getInt();
@@ -58,10 +109,20 @@ public class DurableListNodeData {
         rightNode = (LazyDurableListNode) f.deserialize(byteBuffer);
     }
 
+    /**
+     * Returns true if this is the data for the nil node.
+     *
+     * @return True if nil node.
+     */
     public boolean isNil() {
         return level == 0;
     }
 
+    /**
+     * Returns the length of the serialized data, including the id and durable length.
+     *
+     * @return The length of the serialized data.
+     */
     public int getDurableLength() {
         if (isNil())
             return 2;
@@ -86,6 +147,12 @@ public class DurableListNodeData {
         rightNode.writeDurable(byteBuffer);
     }
 
+    /**
+     * Returns true if the value of the node exists for the given time.
+     *
+     * @param time    The time of the query.
+     * @return True if the value currently exists.
+     */
     public boolean exists(long time) {
         return time >= created && time < deleted;
     }
