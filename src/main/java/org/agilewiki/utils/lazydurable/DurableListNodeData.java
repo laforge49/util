2495b9e94c1b1da67d3fdfa596cf0c8492a7ba66
Bill La Forge <laforge49@gmail.com>
2015-03-15 07:05:56 -0400

    #14 added some logic to list data

--- src/main/java/org/agilewiki/utils/lazydurable/DurableListNodeData.java
+++ src/main/java/org/agilewiki/utils/lazydurable/DurableListNodeData.java
@@ -1,6 +1,7 @@
 package org.agilewiki.utils.lazydurable;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 /**
  * The durable data elements of a list node.
@@ -42,6 +43,11 @@ public class DurableListNodeData {
     public final LazyDurableListNode rightNode;
 
     /**
+     * The node which holds this data.
+     */
+    public final LazyDurableListNode thisNode;
+
+    /**
      * The factory for the value.
      */
     protected final LazyDurableFactory valueFactory;
@@ -49,9 +55,10 @@ public class DurableListNodeData {
     /**
      * Create the nil node data.
      *
-     * @param thisNode    The nil node.
+     * @param thisNode The node which holds this data.
      */
     public DurableListNodeData(LazyDurableListNode thisNode) {
+        this.thisNode = thisNode;
         this.level = 0;
         totalSize = 0;
         this.created = 0L;
@@ -66,21 +73,24 @@ public class DurableListNodeData {
     /**
      * Create non-nill node data.
      *
-     * @param level        Composite node depth--see AA Tree algorithm.
-     * @param totalSize    Number of nodes in this subtree.
-     * @param created      Creation time of this node.
-     * @param deleted      Deletion time of this node.
-     * @param leftNode     Left subtree node.
-     * @param value        The value of the node.
-     * @param rightNode    Right subtree node.
+     * @param thisNode  The node which holds this data.
+     * @param level     Composite node depth--see AA Tree algorithm.
+     * @param totalSize Number of nodes in this subtree.
+     * @param created   Creation time of this node.
+     * @param deleted   Deletion time of this node.
+     * @param leftNode  Left subtree node.
+     * @param value     The value of the node.
+     * @param rightNode Right subtree node.
      */
-    public DurableListNodeData(int level,
+    public DurableListNodeData(LazyDurableListNode thisNode,
+                               int level,
                                int totalSize,
                                long created,
                                long deleted,
                                LazyDurableListNode leftNode,
                                Object value,
                                LazyDurableListNode rightNode) {
+        this.thisNode = thisNode;
         this.level = level;
         this.totalSize = totalSize;
         this.created = created;
@@ -94,9 +104,11 @@ public class DurableListNodeData {
     /**
      * Create non-nill node data.
      *
-     * @param byteBuffer    Holds the serialized data.
+     * @param thisNode   The node which holds this data.
+     * @param byteBuffer Holds the serialized data.
      */
-    public DurableListNodeData(ByteBuffer byteBuffer) {
+    public DurableListNodeData(LazyDurableListNode thisNode, ByteBuffer byteBuffer) {
+        this.thisNode = thisNode;
         level = byteBuffer.getInt();
         totalSize = byteBuffer.getInt();
         created = byteBuffer.getLong();
@@ -150,10 +162,263 @@ public class DurableListNodeData {
     /**
      * Returns true if the value of the node exists for the given time.
      *
-     * @param time    The time of the query.
+     * @param time The time of the query.
      * @return True if the value currently exists.
      */
     public boolean exists(long time) {
         return time >= created && time < deleted;
     }
+
+    /**
+     * Returns the count of all the values currently in the list.
+     *
+     * @param time The time of the query.
+     * @return The current size of the list.
+     */
+    public int size(long time) {
+        if (isNil())
+            return 0;
+        int s = leftNode.size(time) + rightNode.size(time);
+        if (exists(time))
+            s += 1;
+        return s;
+    }
+
+    /**
+     * Returns the selected node.
+     *
+     * @param ndx Relative position of the selected node within the sublist.
+     * @return The selected node, or null.
+     */
+    public LazyDurableListNode getListNode(int ndx) {
+        if (ndx < 0 || ndx >= totalSize)
+            return null; //out of range
+        int leftSize = leftNode.totalSize();
+        if (ndx < leftSize)
+            return leftNode.getData().getListNode(ndx);
+        if (ndx > leftSize)
+            return rightNode.getData().getListNode(ndx - leftSize - 1);
+        return thisNode;
+    }
+
+    /**
+     * Returns the value if it exists.
+     *
+     * @param time The time of the query.
+     * @return The value, or null.
+     */
+    public Object getExistingValue(long time) {
+        return exists(time) ? value : null;
+    }
+
+    /**
+     * Get the index of an existing value with the same identity (==).
+     * (The list is searched in order.)
+     *
+     * @param value The value sought.
+     * @param time  The time of the query.
+     * @return The index, or -1.
+     */
+    public int getIndex(Object value, long time) {
+        if (isNil())
+            return -1;
+        int ndx = leftNode.getIndex(value, time);
+        if (ndx > -1)
+            return ndx;
+        if (this.value == value && exists(time))
+            return leftNode.totalSize();
+        ndx = rightNode.getIndex(value, time);
+        if (ndx == -1)
+            return -1;
+        return leftNode.totalSize() + 1 + ndx;
+    }
+
+    /**
+     * Get the index of an existing value with the same identity (==).
+     * (The list is searched in reverse order.)
+     *
+     * @param value The value sought.
+     * @param time  The time of the query.
+     * @return The index, or -1.
+     */
+    public int getIndexRight(Object value, long time) {
+        if (isNil())
+            return -1;
+        int ndx = rightNode.getIndexRight(value, time);
+        if (ndx > -1)
+            return leftNode.totalSize() + 1 + ndx;
+        if (this.value == value && exists(time))
+            return leftNode.totalSize();
+        ndx = leftNode.getIndexRight(value, time);
+        if (ndx == -1)
+            return -1;
+        return ndx;
+    }
+
+    /**
+     * Find the index of an equal existing value.
+     * (The list is searched in order.)
+     *
+     * @param value The value sought.
+     * @param time  The time of the query.
+     * @return The index, or -1.
+     */
+    public int findIndex(Object value, long time) {
+        if (isNil())
+            return -1;
+        int ndx = leftNode.findIndex(value, time);
+        if (ndx > -1)
+            return ndx;
+        if (exists(time) && this.value.equals(value))
+            return leftNode.totalSize();
+        ndx = rightNode.findIndex(value, time);
+        if (ndx == -1)
+            return -1;
+        return leftNode.totalSize() + 1 + ndx;
+    }
+
+    /**
+     * Find the index of an equal existing value.
+     * (The list is searched in reverse order.)
+     *
+     * @param value The value sought.
+     * @param time  The time of the query.
+     * @return The index, or -1.
+     */
+    public int findIndexRight(Object value, long time) {
+        if (isNil())
+            return -1;
+        int ndx = rightNode.findIndexRight(value, time);
+        if (ndx > -1)
+            return leftNode.totalSize() + 1 + ndx;
+        if (exists(time) && this.value.equals(value))
+            return leftNode.totalSize();
+        ndx = leftNode.findIndexRight(value, time);
+        if (ndx == -1)
+            return -1;
+        return ndx;
+    }
+
+    /**
+     * Returns the index of an existing value higher than the given index.
+     *
+     * @param ndx  A given index.
+     * @param time The time of the query.
+     * @return An index of an existing value that is higher, or -1.
+     */
+    public int higherIndex(int ndx, long time) {
+        if (ndx >= totalSize - 1 || isNil())
+            return -1; //out of range
+        int leftSize = leftNode.totalSize();
+        if (ndx < leftSize - 1) {
+            int h = leftNode.higherIndex(ndx, time);
+            if (h > -1)
+                return h;
+        }
+        if (ndx < leftSize) {
+            if (exists(time))
+                return leftSize;
+        }
+        int h = rightNode.higherIndex(ndx - leftSize - 1, time);
+        return h == -1 ? -1 : h + leftSize + 1;
+    }
+
+    /**
+     * Returns the index of an existing value higher than or equal to the given index.
+     *
+     * @param ndx  A given index.
+     * @param time The time of the query.
+     * @return An index of an existing value that is higher or equal, or -1.
+     */
+    public int ceilingIndex(int ndx, long time) {
+        if (ndx >= totalSize || isNil()) {
+            return -1; //out of range
+        }
+        int leftSize = leftNode.totalSize();
+        if (ndx < leftSize) {
+            int h = leftNode.ceilingIndex(ndx, time);
+            if (h > -1)
+                return h;
+        }
+        if (ndx <= leftSize) {
+            if (exists(time))
+                return leftSize;
+        }
+        int h = rightNode.ceilingIndex(ndx - leftSize - 1, time);
+        return h <= -1 ? -1 : h + leftSize + 1;
+    }
+
+    /**
+     * Returns the index of an existing value lower than the given index.
+     *
+     * @param ndx  A given index.
+     * @param time The time of the query.
+     * @return An index of an existing value that is lower, or -1.
+     */
+    public int lowerIndex(int ndx, long time) {
+        if (ndx <= 0 || isNil())
+            return -1; //out of range
+        int leftSize = leftNode.totalSize();
+        if (ndx > leftSize + 1) {
+            int l = rightNode.lowerIndex(ndx - leftSize - 1, time);
+            if (l > -1)
+                return l + leftSize + 1;
+        }
+        if (ndx > leftSize) {
+            if (exists(time))
+                return leftSize;
+        }
+        return leftNode.lowerIndex(ndx, time);
+    }
+
+    /**
+     * Returns the index of an existing value lower than or equal to the given index.
+     *
+     * @param ndx  A given index.
+     * @param time The time of the query.
+     * @return An index of an existing value that is lower or equal, or -1.
+     */
+    public int floorIndex(int ndx, long time) {
+        if (ndx < 0 || isNil())
+            return -1; //out of range
+        int leftSize = leftNode.totalSize();
+        if (ndx > leftSize) {
+            int l = rightNode.floorIndex(ndx - leftSize - 1, time);
+            if (l > -1)
+                return l + leftSize + 1;
+        }
+        if (ndx >= leftSize) {
+            if (exists(time))
+                return leftSize;
+        }
+        return leftNode.floorIndex(ndx, time);
+    }
+
+    /**
+     * Returns true if there are no values present for the given time.
+     *
+     * @param time The time of the query.
+     * @return Returns true if the list is empty for the given time.
+     */
+    public boolean isEmpty(long time) {
+        if (isNil())
+            return true;
+        return !(exists(time) && leftNode.isEmpty(time) && rightNode.isEmpty(time));
+    }
+
+    /**
+     * Appends existing values to the list.
+     *
+     * @param list    The list being build.
+     * @param time    The time of the query.
+     */
+    public void flatList(List list, long time) {
+        if (isNil())
+            return;
+        leftNode.getData().flatList(list, time);
+        if (exists(time))
+            list.add(value);
+        rightNode.getData().flatList(list, time);
+    }
 }
+
