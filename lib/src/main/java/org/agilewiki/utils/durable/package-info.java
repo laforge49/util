ee055bad4c950957d62d7ce64ff779dd72642bb1
Bill La Forge <laforge49@gmail.com>
2015-03-20 15:28:25 -0400

    #18 dropped older versions of immutables

--- lib/src/main/java/org/agilewiki/utils/durable/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <p>
- *     This version of immutable versioning map list supports serialization, along with
- *     several other immutable classes like String and Float.
- * </p>
- */
-package org.agilewiki.utils.durable;
--- lib/src/main/java/org/agilewiki/utils/lazydurable/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <p>
- *     Lazy deserialization and smart reserialization are used to speed the processing
- *     of immutable versioning map lists..
- * </p>
- */
-package org.agilewiki.utils.lazydurable;
