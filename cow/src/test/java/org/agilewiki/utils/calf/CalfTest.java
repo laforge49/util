d6f2cafae82680f641b06f226b3dcf71d0d47c48
Bill La Forge <laforge49@gmail.com>
2015-03-25 09:54:24 -0400

    #24 DSMTest works

---
+++ cow/src/test/java/org/agilewiki/utils/calf/CalfTest.java
@@ -0,0 +1,37 @@
+package org.agilewiki.utils.calf;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor2.core.impl.Plant;
+import org.agilewiki.utils.immutable.Registry;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CalfTest extends TestCase {
+    public void test() throws Exception {
+        new Plant();
+        try {
+            Registry registry = new Registry();
+            Path calfPath = Paths.get("calf.db");
+            int maxRootBlockSize = 1000;
+            try (Db calf = new Db(registry, calfPath, maxRootBlockSize)) {
+                Files.deleteIfExists(calfPath);
+                calf.open(true, "!");
+                Transaction t2 = new Transaction() {
+                    @Override
+                    public Object transform(Object immutable) {
+                        return "hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+                    }
+                };
+                calf.update(t2).call();
+                calf.close();
+                System.out.println(Files.size(calfPath));
+                calf.open();
+                System.out.println(calf.immutable);
+            }
+        } finally {
+            Plant.close();
+        }
+    }
+}
