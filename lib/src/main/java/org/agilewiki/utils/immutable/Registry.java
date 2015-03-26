5a261b8619999adaaa748265f54f48231c05040f
Bill La Forge <laforge49@gmail.com>
2015-03-26 07:55:39 -0400

    #25 new base registry for "scalars"

--- lib/src/main/java/org/agilewiki/utils/immutable/Registry.java
+++ lib/src/main/java/org/agilewiki/utils/immutable/Registry.java
@@ -6,7 +6,7 @@ import org.agilewiki.utils.immutable.scalars.*;
 /**
  * Initialize the factory registry.
  */
-public class Registry extends CascadingRegistry {
+public class Registry extends BaseRegistry {
     public final VersionedListNode nilVersionedList;
     public final VersionedMapNode nilVersionedMap;
     public final ListNode nilList;
@@ -16,16 +16,8 @@ public class Registry extends CascadingRegistry {
      * Creates the registry and registers the default factories.
      */
     public Registry() {
-        new NullFactory(this, NULL_ID); // 'N'
-        new StringFactory(this, 'S');
-        new DoubleFactory(this, 'D');
-        new BooleanFactory(this, 'B', 't', 'f');
-        new FloatFactory(this, 'F');
-        new IntegerFactory(this, 'I');
-        new LongFactory(this, 'L');
         nilVersionedList = new VersionedListNodeFactory(this, 'l', '1').nilVersionedList;
         nilVersionedMap = new VersionedMapNodeFactory(this, 'm', '2', nilVersionedList).nilVersionedMap;
-        new CS256Factory(this, 'c');
         nilList = new ListNodeFactory(this, 'n', '3').nilList;
         nilMap = new MapNodeFactory(this, 'o', '4', nilList).nilMap;
     }
