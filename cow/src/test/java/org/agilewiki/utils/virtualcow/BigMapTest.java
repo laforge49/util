675c10f71d06e5cba9f3b1e4286a77be69eb9c9e
Bill La Forge <laforge49@gmail.com>
2015-03-30 14:11:01 -0400

    #30 db fails when too big

--- cow/src/test/java/org/agilewiki/utils/virtualcow/BigMapTest.java
+++ cow/src/test/java/org/agilewiki/utils/virtualcow/BigMapTest.java
@@ -10,35 +10,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class DbTest extends TestCase {
+public class BigMapTest extends TestCase {
     public void test() throws Exception {
         new Plant();
         try {
             Path dbPath = Paths.get("vcow.db");
             Files.deleteIfExists(dbPath);
-            int maxRootBlockSize = 1000;
-            try (Db db = new Db(new BaseRegistry(), dbPath, maxRootBlockSize)) {
+            int maxBlockSize = 1000;
+            try (Db db = new Db(new BaseRegistry(), dbPath, maxBlockSize)) {
                 Files.deleteIfExists(dbPath);
                 db.open(true);
                 Transaction t2 = new Transaction() {
                     @Override
                     public MapNode transform(MapNode mapNode)
                             throws IOException {
-                        System.out.println("block usage: " + db.usage());
-                        mapNode = mapNode.add("x", "hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-
-                        BlockReference blockReference = new BlockReference(db, "ho!");
-                        mapNode = mapNode.add("y", blockReference);
+                        for (int i = 0; i < 22; i++) {
+                            mapNode = mapNode.add(i, "");
+                        }
+                        System.out.println(mapNode.getDurableLength());
                         return mapNode;
                     }
                 };
                 db.update(t2).call();
                 db.close();
-                System.out.println(Files.size(dbPath));
-                db.open();
-                System.out.println(db.mapNode.firstKey());
-                BlockReference br = (BlockReference) db.mapNode.listAccessor("y").get(0);
-                System.out.println(br.get());
             }
         } finally {
             Plant.close();
