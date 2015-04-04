fda9fc849ab65af2529b6b93d0f3e30df29e0ffc
Bill La Forge <laforge49@gmail.com>
2015-04-04 18:11:00 -0400

    #32 completed cleanup

--- cow/src/main/java/org/agilewiki/utils/virtualcow/collections/SerializationException.java
+++
@@ -1,9 +0,0 @@
-package org.agilewiki.utils.virtualcow.collections;
-
-import org.agilewiki.utils.BlockIOException;
-
-/**
- * Thrown when the number of bytes serialized is not the durable length.
- */
-public class SerializationException extends BlockIOException {
-}
