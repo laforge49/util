d228b78044555a6be27bfc36e36367fcdf4b2438
Bill La Forge <laforge49@gmail.com>
2015-04-11 15:03:46 -0400

    #43 cleanup

--- cow/src/test/java/org/agilewiki/utils/virtualcow/SecondaryTran.java
+++ cow/src/test/java/org/agilewiki/utils/virtualcow/SecondaryTran.java
@@ -8,32 +8,33 @@ import org.agilewiki.utils.immutable.collections.*;
 public class SecondaryTran implements Transaction {
     @Override
     public void transform(Db db, MapNode tMapNode) {
+        long timestamp = db.getTimestamp();
         String johnJonesId = NameId.generate("JohnJones");
         String JohnJonesVId = ValueId.generate("John Jones");
         String nameTId = NameId.generate("name");
         String johnJonesSID = SecondaryId.secondaryId(nameTId, JohnJonesVId);
         SecondaryId.createSecondaryId(db, johnJonesId, johnJonesSID);
-        Display.all(db, db.getTimestamp());
+        Display.all(db, timestamp);
 
         System.out.println("\nSecondary ids of "+johnJonesId);
         VersionedMapNode vmn = db.versionedMapNode(johnJonesId);
-        System.out.println("    " + SecondaryId.secondaryIdList(vmn, db.getTimestamp()));
+        System.out.println("    " + SecondaryId.secondaryIdList(vmn, timestamp));
 
         System.out.println("\nVMN IDs for secondary id " + johnJonesSID);
-        for (String vmlId: SecondaryId.vlnIdIterable(db, johnJonesSID, db.getTimestamp())) {
+        for (String vmlId: SecondaryId.vlnIdIterable(db, johnJonesSID, timestamp)) {
             System.out.println("    "+vmlId);
         }
 
         SecondaryId.removeSecondaryId(db, johnJonesId, johnJonesSID);
-        Display.all(db, db.getTimestamp());
+        Display.all(db, timestamp);
 
         System.out.println("\nSecondary ids of " + johnJonesId);
         vmn = db.versionedMapNode(johnJonesId);
-        System.out.println("    " + SecondaryId.secondaryIdList(vmn, db.getTimestamp()));
+        System.out.println("    " + SecondaryId.secondaryIdList(vmn, timestamp));
 
 
         System.out.println("\nVMN IDs for secondary id " + johnJonesSID);
-        for (String vmlId: SecondaryId.vlnIdIterable(db, johnJonesSID, db.getTimestamp())) {
+        for (String vmlId: SecondaryId.vlnIdIterable(db, johnJonesSID, timestamp)) {
             System.out.println("    "+vmlId);
         }
     }
