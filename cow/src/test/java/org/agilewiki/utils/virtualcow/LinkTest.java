8b0c397755337a4f7f08ce3f12416603e5f0b462
Bill La Forge <laforge49@gmail.com>
2015-04-13 14:34:43 -0400

    #47 LinkTest

---
+++ cow/src/test/java/org/agilewiki/utils/virtualcow/LinkTest.java
@@ -0,0 +1,28 @@
+package org.agilewiki.utils.virtualcow;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor2.core.impl.Plant;
+import org.agilewiki.utils.immutable.BaseRegistry;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class LinkTest extends TestCase {
+    public void test() throws Exception {
+        new Plant();
+        try {
+            Path dbPath = Paths.get("vcow.db");
+            Files.deleteIfExists(dbPath);
+            int maxRootBlockSize = 1000;
+            try (Db db = new Db(new BaseRegistry(), dbPath, maxRootBlockSize)) {
+                Files.deleteIfExists(dbPath);
+                db.registerTransaction("LinkTran", LinkTran.class);
+                db.open(true);
+                String timestampId = db.update("LinkTran").call();
+            }
+        } finally {
+            Plant.close();
+        }
+    }
+}
---
+++ cow/src/test/java/org/agilewiki/utils/virtualcow/LinkTran.java
@@ -0,0 +1,12 @@
+package org.agilewiki.utils.virtualcow;
+
+import org.agilewiki.utils.ids.NameId;
+import org.agilewiki.utils.ids.ValueId;
+import org.agilewiki.utils.ids.composites.SecondaryId;
+import org.agilewiki.utils.immutable.collections.MapNode;
+
+public class LinkTran implements Transaction {
+    @Override
+    public void transform(Db db, MapNode tMapNode) {
+    }
+}
